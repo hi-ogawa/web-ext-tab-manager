5512dc565be0781f87df03ec54572183d31f8736
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-19 19:28:34 +0900

    fix: fix logger in service worker

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,11 +1,21 @@
 import { Roarr, ROARR } from "roarr";
 import { createLogWriter } from "@roarr/browser-log-writer";
+import { tinyassert } from "@hiogawa/utils";
 
 // https://github.com/gajus/roarr
 
 export const logger = Roarr;
 
 export function initializeLogger() {
-  window.localStorage.setItem("ROARR_LOG", "true");
-  ROARR.write = createLogWriter();
+  ROARR.write = createLogWriter({
+    // fake `window.localStorage.setItem("ROARR_LOG", "true")` for service worker
+    storage: {
+      getItem: (name: string): string | null => {
+        return name === "ROARR_LOG" ? "true" : null;
+      },
+      setItem: (_name: string, _value: string): void => {
+        tinyassert(false, "unused");
+      },
+    },
+  });
 }
