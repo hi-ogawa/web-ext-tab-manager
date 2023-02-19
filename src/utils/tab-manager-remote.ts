36f19d53f57c36fe553dc5ac317b49975cdfb1c2
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-19 16:16:33 +0900

    refactor: use eventemitter3

--- src/utils/tab-manager-remote.ts
+++ src/utils/tab-manager-remote.ts
@@ -5,5 +5,6 @@ import { CONNECT_TAB_MANAGER, TabManager } from "./tab-manager";
 export let tabManagerRemote: Remote<TabManager>;
 
 export async function initializeTabManagerRemote() {
+  // TODO: check comlink is ready (e.g. by dedicated "ping" call?)
   tabManagerRemote = createComlinkProxy<TabManager>(CONNECT_TAB_MANAGER);
 }
