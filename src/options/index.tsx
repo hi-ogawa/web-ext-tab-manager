e943c8b8a45f55b82407dd5000e34d5248462593
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-24 00:09:43 +0900

    chore: tsx too

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,9 +1,9 @@
-import "../styles/index.ts";
-import { createRoot } from "react-dom/client";
 import { tinyassert } from "@hiogawa/utils";
-import { App } from "./app";
-import { initializeTabManagerRemote } from "../utils/tab-manager-remote";
+import { createRoot } from "react-dom/client";
+import "../styles/index.ts";
 import { initializeLogger } from "../utils/logger";
+import { initializeTabManagerRemote } from "../utils/tab-manager-remote";
+import { App } from "./app";
 
 async function main() {
   initializeLogger();
