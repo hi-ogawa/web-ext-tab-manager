d1861f2f81dbee775b01cfea1bd38a0e31f76b6c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-23 17:15:52 +0900

    chore: refactor config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import process from "node:process";
-import { PlaywrightTestConfig } from "@playwright/test";
+import { defineConfig } from "@playwright/test";
 
-const config: PlaywrightTestConfig = {
+export default defineConfig({
   testDir: "./e2e",
   use: {
     trace: process.env["E2E_TRACE"] ? "on" : "off",
@@ -12,6 +12,4 @@ const config: PlaywrightTestConfig = {
     command: `pnpm dev >> e2e.log 2>&1`,
     port: 18181,
   },
-};
-
-export default config;
+});
