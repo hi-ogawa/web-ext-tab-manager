bd2399df0dfb869a976059b30cd1c32b4c96f4c9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-23 19:35:11 +0900

    feat: sort ImportDeclaration

--- misc/jscodeshift/isort.ts
+++ misc/jscodeshift/isort.ts
@@ -1,8 +1,9 @@
 import { tinyassert } from "@hiogawa/utils";
+import type { StatementKind } from "ast-types/gen/kinds";
 import type { API, FileInfo, ImportDeclaration } from "jscodeshift";
 
 // skip transforming node_modules
-const { sortBy } = require("lodash") as typeof import("lodash");
+const { sortBy, range } = require("lodash") as typeof import("lodash");
 
 //
 // usage:
@@ -13,10 +14,14 @@ export default function transformer(file: FileInfo, api: API) {
   const j = api.jscodeshift;
   const $j = j(file.source);
 
-  // TODO: how split `ImportDeclaration` into sections by continuous lines?
-  for (const p of $j.find(j.ImportDeclaration).paths()) {
-    const node = p.value;
-    sortImportSpecifiers(node);
+  // sort Program.body array
+  const program = $j.find(j.Program).nodes()[0];
+  tinyassert(program);
+  program.body = sortStatements(program.body);
+
+  // sorty ImportDeclaration.specifiers array
+  for (const decl of $j.find(j.ImportDeclaration).nodes()) {
+    sortImportSpecifiers(decl);
   }
 
   return $j.toSource();
@@ -25,7 +30,36 @@ export default function transformer(file: FileInfo, api: API) {
   // helper
   //
 
-  // mutates AST
+  function sortStatements(statements: StatementKind[]): StatementKind[] {
+    // group neighboring statements
+    const groups = groupNeighborBy(
+      statements,
+      (stmt) => stmt.type === "ImportDeclaration"
+    );
+
+    // sort each ImportDeclaration group
+    for (const i of range(groups.length)) {
+      const group = groups[i]!;
+      if (group[0]?.type !== "ImportDeclaration") {
+        continue;
+      }
+      groups[i] = sortImportDeclarations(groups[i] as ImportDeclaration[]);
+    }
+
+    // concat groups
+    return groups.flat();
+  }
+
+  function sortImportDeclarations(
+    decls: ImportDeclaration[]
+  ): ImportDeclaration[] {
+    return sortBy(decls, (decl) => {
+      tinyassert(j.StringLiteral.check(decl.source));
+      return decl.source.value;
+    });
+  }
+
+  // mutate
   function sortImportSpecifiers(decl: ImportDeclaration) {
     if (!decl.specifiers) {
       return;
@@ -38,4 +72,23 @@ export default function transformer(file: FileInfo, api: API) {
       return node.imported.name;
     });
   }
+}
+
+//
+// utils
+//
+
+function groupNeighborBy<T, K>(ls: T[], f: (x: T) => K): T[][] {
+  if (ls.length === 0) {
+    return [];
+  }
+  const groups: T[][] = [[ls.shift()!]];
+  for (const x of ls) {
+    if (f(x) === f(groups.at(-1)![0]!)) {
+      groups.at(-1)!.push(x);
+    } else {
+      groups.push([x]);
+    }
+  }
+  return groups;
 }
